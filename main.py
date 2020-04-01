2dec999902e9a0bc24fd70ac2355e8444bf31ece
ross <rosenlof.ross@gmail.com>
2020-04-01 10:45:21 -0600

    splitting up functions, get pal from img

--- main.py
+++ main.py
@@ -23,8 +23,20 @@ def makePalette(array_colors):
     color_c += 3
     rgb_image.paste(im,((i)*THUMBNAIL_WIDTH,0), mask=None)
     
-  rgb_image.save('swatch.jpeg')
+  rgb_image.save('swatch.png')
   return rgb_image
+
+def getPaletteOfImg(imgPath):
+  img = Image.open(imgPath)
+
+  newPalette = []
+  palette = Haishoku.getPalette(imgPath)
+
+  for i in range(0,len(palette)):
+    for j in palette[i][1]:
+      newPalette.append(j)
+  print('   Palette: {}'.format(newPalette))
+  return newPalette
 
 def quantizetopalette(silf, palette, dither=False):
     """Convert an RGB or L mode image to use a given P image's palette."""
@@ -48,7 +60,7 @@ def quantizetopalette(silf, palette, dither=False):
     except AttributeError:
         return silf._makeself(im)
 
-def pixelate(g, dir, new_dir, contrast_val, color_val):
+def pixelate(g, dir, new_dir, contrast_val, color_val, palette=None):
 
   for filename in os.listdir(dir):
     imgPath = os.path.join(('{}/'.format(dir)),filename)
@@ -72,27 +84,27 @@ def pixelate(g, dir, new_dir, contrast_val, color_val):
 
     # resize contrasted image and save it
     contrast = contrast.resize((resize_h,resize_w),resample=Image.BILINEAR)
-    contrastFileName = "pxl_{}_{}".format(g,filename)
+    contrastFileName = "pxl_{}_{}.png".format(g,os.path.splitext(filename)[0])
     contrastImgPath = os.path.join(('{}/'.format(new_dir)),contrastFileName)
     contrast.save(contrastImgPath, "PNG")
     newContrastImg = Image.open(contrastImgPath)
 
-    # get palette of new image
-    palette = Haishoku.getPalette(contrastImgPath)
-
-    # change format of palette to remove percents
-    newPalette = []
-    for i in range(0,len(palette)):
-      for j in palette[i][1]:
-        newPalette.append(j)
-    print('   Palette: {}'.format(newPalette))
-
     # shrink image to create pixels
     imgSmall = contrast.resize((resize_w,resize_h),resample=Image.BILINEAR)
+    
+    img2 = Image.new('P', (16,16))
+    newPalette = []
+
+    # get palette of new image or use the args palette
+    if palette is None:
+      newPalette = getPaletteOfImg(contrastImgPath)
+      img2.putpalette(newPalette * 32)
+    else:
+      numcolors = len(palette) / 3
+      mult = int(256 / numcolors)
+      img2.putpalette(palette * mult)
 
     # change color of the image
-    img2 = Image.new('P', (16,16))
-    img2.putpalette(newPalette * 32)
     img2 = quantizetopalette(imgSmall,img2,dither=False)
     if img2.mode != 'RGB':
       img2 = img2.convert('RGB')
@@ -101,9 +113,10 @@ def pixelate(g, dir, new_dir, contrast_val, color_val):
     result = img2.resize(img.size,Image.NEAREST)
     
     # save the new image
-    newImgFileName = "pxl_{}_{}".format(g,filename)
+    newImgFileName = "pxl_{}_{}.png".format(g,os.path.splitext(filename)[0])
     newImgPath = os.path.join(('{}/'.format(new_dir)),newImgFileName)
     print('Saving new image: {}'.format(newImgPath))
+    print('type: {}'.format(type(result)))
     result.save(newImgPath, "PNG")
 
 def contrast_img(img, contrast_val=1.0, color_val=1.0):
@@ -116,17 +129,17 @@ def contrast_img(img, contrast_val=1.0, color_val=1.0):
   return contrast
 
 # Random test array (4):
-# ARRAY_COLORS = [0, 0, 4, 127, 149, 127, 40, 187, 40, 33, 33, 240]
+# ARRAY_COLORS = [0, 0, 4, 127, 149, 127, 40, 187, 40, 33, 33, 240,0,89,166, 0,0,0, 249,243,167]
 
 # Rio Colors Post It collection (5):
-#ARRAY_COLORS = [255,153,42,255,104,180,51,153,254,108,219,108,255,212,45]
+# ARRAY_COLORS = [255,153,42,255,104,180,51,153,254,108,219,108,255,212,45]
 
 # Starry Night Colors (8):
 ARRAY_COLORS = [0,89,166,51,153,254,83,211,230,0,180,143,255,212,45,249,243,167,0,0,0,255,255,255]
 
+pal = getPaletteOfImg('images/kershisnik.jpg')
+makePalette(pal)
 
 # p = makePalette(ARRAY_COLORS)
-pixelate(g=128,dir='images',new_dir='new_images',contrast_val=1.7,color_val=2.0) 
-pixelate(g=72,dir='images',new_dir='new_images',contrast_val=1.7,color_val=2.0) 
-# pixelate(g=36,dir='images',new_dir='new_images',contrast_val=1.7,color_val=2.0) 
-# pixelate(g=18,dir='images',new_dir='new_images',contrast_val=1.7,color_val=2.0) 
+# pixelate(g=360,dir='images',new_dir='new_images',contrast_val=1.0,color_val=1.0,palette=pal)
+pixelate(g=160,dir='single_images',new_dir='new_images',contrast_val=1.0,color_val=1.0,palette=pal) 
